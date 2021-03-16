9472bde8b80f105156dc5636d577fb4711b933d5
Hassan Ali <hassanalimalek9@gmail.com>
2021-03-16 13:52:13 +0500

    Note Storage issue fixed

--- index.js
+++ index.js
@@ -11,18 +11,7 @@ let note_edit_form = document.querySelector('.note_edit_form')
 let note_edit_overlay = document.querySelector('.note_edit_overlay');
 let note_changes_submit = document.querySelector('.note_changes_submit');
 let loading_animation = document.querySelector('.loading_animation');
-let example_note_date= document.querySelector('.example_note_date')
 
-
-// Getting Notes from local Storage.
-var saved_notes = localStorage.getItem('allNotes');
-if(saved_notes){
-  notes_row.innerHTML = saved_notes;
-}
-// Animation Close timeout
-setTimeout(function(){
-  loading_animation.style.display = 'none';
-},2000)
 
 // Get current date ()
 let get_date =()=>{
@@ -30,64 +19,112 @@ let get_date =()=>{
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 return `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
 }
-// Setting example note date.
-let curr_date = get_date();
-example_note_date.innerText=curr_date;
+
+// Show latest notes func.
+let showNotes = ()=>{
+  let cur_date = get_date();
+  saved_notes_arr = localStorage.getItem("allnotes");
+  notes_row.innerHTML = '';
+  JSON.parse(saved_notes_arr).map((note_obj)=>{
+    let new_note = `<div class="col-12 col-sm-6 col-md-4 mb-4">
+  <div class="card">
+      <div class="card-header">
+        <div class="card-header_title">
+          <h3>${note_obj.title}</h3>
+          <div class="card-header_icons">
+          <i id = ${note_obj.id} class="far fa-edit edit_icon note_icon"></i>
+          <i id = ${note_obj.id} class="fas fa-trash-alt note_icon delete_icon"></i>
+        </div>
+        </div>
+      </div>
+      <div class="card-body">
+        <blockquote class="blockquote mb-0">
+          <p style="white-space: pre-line">${note_obj.description}</p>
+          <footer class="blockquote-footer mt-2">${cur_date}</footer>
+        </blockquote>
+      </div>
+    </div>
+  </div>`
+    notes_row.insertAdjacentHTML("afterbegin", new_note);
+  })
+  
+}
+
+
+// Getting Notes from local Storage.
+var saved_notes_arr = JSON.parse(localStorage.getItem('allNotes'));
+if(saved_notes_arr){
+  showNotes();
+}
+else{
+  let obj = {
+    id:'test',
+    title:'Test Note',
+    description:`Your go-to app for taking notes 
+    1) Click on plus icon to add a new note.
+    2) Click on edit icon to edit a note.
+    3) Trash Icon to delete a note`
+  }
+  let arr = [obj];
+  localStorage.setItem("allnotes",JSON.stringify(arr));
+  showNotes();
+  
+}
+
+// Animation Close timeout
+setTimeout(function(){
+  loading_animation.style.display = 'none';
+},2000)
+
 
 // Edit Note and Delete Note
-let edit_note_ref = null;
+let edit_note_id = null;
 notes_row.addEventListener('click', (e)=>{
-  console.log("Clicked note edit ")
+  saved_notes_arr = JSON.parse(localStorage.getItem("allnotes"));
+  edit_note_id = (e.target.id);
   if(e.target.classList.contains('fa-edit')){
-    console.log("Edit Clicked!!!")
-    edit_note_ref = (e.target.parentElement.parentElement.parentElement.parentElement);
-    let description = e.target.parentElement.parentElement.parentElement.parentElement.lastElementChild.firstElementChild.firstElementChild.innerText;
-    let title = e.target.parentElement.parentElement.firstElementChild.innerText;
-    note_edit_form.note_title.value = title;
-    note_edit_form.note_description.value = description;
+    let found_note = saved_notes_arr.find((note_obj)=>{
+      return note_obj.id === edit_note_id;
+    })
+    note_edit_form.note_title.value = found_note.title;
+    note_edit_form.note_description.value = found_note.description;
     note_edit_overlay.style.display='block'
   }
   if(e.target.classList.contains('fa-trash-alt')){
-    e.target.parentElement.parentElement.parentElement.parentElement.parentElement.remove();
-    let notes_row = document.querySelector('.notes_row');
-    localStorage.setItem("allNotes",notes_row.innerHTML);
+    
+    // Finding Index of object to make changes
+    let objIndex = saved_notes_arr.findIndex((obj)=>obj.id == edit_note_id);
+    // Making Changes
+    saved_notes_arr.splice(objIndex, 1);
+    localStorage.setItem("allnotes",JSON.stringify(saved_notes_arr));
+    showNotes();
   }
 })
+
 // Note Changes Submission
 note_changes_submit.addEventListener('click',(e)=>{
   e.preventDefault();
+  // User Inputs
   let note_title = note_edit_form.note_title.value;
   let note_description = note_edit_form.note_description.value;
-  edit_note_ref.innerHTML = `<div class="card-header">
-  <div class="card-header_title">
-    <h3>${note_title}</h3>
-    <div class="card-header_icons">
-    <i class="far fa-edit edit_icon note_icon"></i>
-    <i class="fas fa-trash-alt note_icon delete_icon"></i>
- </div>
-  </div>
-</div>
-<div class="card-body">
-  <blockquote class="blockquote mb-0">
-    <p style="white-space: pre-line">${note_description}</p>
-    <footer class="blockquote-footer mt-2">15 March</footer>
-  </blockquote>
-</div>`
+  // Array of objects in local storage
+  saved_notes_arr = JSON.parse(localStorage.getItem("allnotes"));
+  // Finding Index of object to make changes
+  let objIndex = saved_notes_arr.findIndex((obj)=>obj.id == edit_note_id);
+  // Making Changes
+  saved_notes_arr[objIndex].title =note_title;
+  saved_notes_arr[objIndex].description =note_description;
+  localStorage.setItem("allnotes",JSON.stringify(saved_notes_arr));
+  showNotes();
   note_edit_overlay.style.display='none';
-  let notes_row = document.querySelector('.notes_row');
-  console.log(notes_row);
-  localStorage.clear();
-  localStorage.setItem("allNotes",notes_row.innerHTML);
 })
 
 
 // Overlay Open & Close
 overlay_exit.addEventListener('click',()=>{
-    console.log("Exit Button Clicked")
     overlay.style.display = 'none';
 })
 add_note_btn.addEventListener('click',()=>{
-  console.log("Plus Clicked")
   overlay.style.display='block'    
 })
 let note_edit_exit_btn  = document.querySelector('.note_edit_exit_btn')
@@ -95,35 +132,24 @@ note_edit_exit_btn.addEventListener('click',()=>{
   note_edit_overlay.style.display='none'
 })
 
+
 // New Note Form Submission
 note_submit_btn.addEventListener('click',(e)=>{
     e.preventDefault();
-    console.log("Submitting Form")
     let note_title = note_form.note_title.value;
     let note_description = note_form.note_description.value;
-    console.log(note_title, note_description)
-    let cur_date = get_date();
-    let new_note = `<div class="col-12 col-sm-6 col-md-4 mb-4">
-    <div class="card">
-        <div class="card-header">
-          <div class="card-header_title">
-            <h3>${note_title}</h3>
-            <div class="card-header_icons">
-            <i class="far fa-edit edit_icon note_icon"></i>
-            <i class="fas fa-trash-alt note_icon delete_icon"></i>
-          </div>
-          </div>
-        </div>
-        <div class="card-body">
-          <blockquote class="blockquote mb-0">
-            <p style="white-space: pre-line">${note_description}</p>
-            <footer class="blockquote-footer mt-2">${cur_date}</footer>
-          </blockquote>
-        </div>
-      </div>
-    </div>`
-    notes_row.insertAdjacentHTML("afterbegin", new_note);
-    localStorage.setItem("allNotes",notes_row.innerHTML);
+    let randNumber = Math.round(Math.random() * 999);
+    let id = note_title + randNumber;
+    let obj = {
+      id: id,
+      title: note_title,
+      description: note_description
+    }
+    saved_notes_arr = JSON.parse(localStorage.getItem("allnotes"));
+    let arr = (saved_notes_arr);
+    arr.push(obj);
+    localStorage.setItem("allnotes",JSON.stringify(arr));
+    showNotes();
     overlay.style.display = 'none';
     note_form.reset();
 })
